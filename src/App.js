100f82eb0b6bd947b67cbcb481bfd77e17ddfd03
Marco <114459798+marcodillanes@users.noreply.github.com>
2023-03-09 17:47:15 -0800

    completed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react'
+import ColorBlock from './ColorBlock';
+import ColorForm from './ColorForm';
 
 function App() {
+  let [colors, setColors] = useState (['violet', 'blue', 'lightblue', 'green', 'greenyellow', 'yellow', 'orange', 'red'])
+
+  let colorMap = colors.map((color, i) => {
+    return (
+      <colorBlock color={color} />
+    )
+  })
+
+  const addColor = (newColor) => {
+    setColors([colors, newColor])
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
+      {colorMap}
+      <ColorForm addColor={addColor} />
+      {
+      /*after running npm start, delete highlighted code */
+      /* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,7 +36,7 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
     </div>
   );
 }
